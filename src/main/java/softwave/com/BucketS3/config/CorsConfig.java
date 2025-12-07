f3de74ef1dafe54beab21ff3681a34508df2289d
Lua ♡ <160372370+lualuaSantosS2@users.noreply.github.com>
2025-12-07 20:37:14 -0300

    Update CorsConfig.java

--- src/main/java/softwave/com/BucketS3/config/CorsConfig.java
+++ src/main/java/softwave/com/BucketS3/config/CorsConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;  
 
 import java.util.Arrays;
 import java.util.List;
